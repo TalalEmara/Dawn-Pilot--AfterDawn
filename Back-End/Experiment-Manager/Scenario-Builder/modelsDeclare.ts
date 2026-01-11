36cda7ca1226f90a8345a3c7e957bc31616da39e
TalalEmara <126427309+TalalEmara@users.noreply.github.com>
2026-01-11 12:38:29 +0200

    busStop declare

--- Back-End/Experiment-Manager/Scenario-Builder/modelsDeclare.ts
+++ Back-End/Experiment-Manager/Scenario-Builder/modelsDeclare.ts
@@ -194,9 +194,8 @@ export const ModelDefinitions: Record<string, ModelDefinition> = {
       Scale: { x: 1, y: 1, z: 1 },
       Model: { url: "/busStop" },
     },
-    normalizeFactors: {
-      Position: { x: 0, y: .302, z: 0 },
-      Scale: { x: 0.001, y: 0.001, z: 0.001 },
+    normalizeFactors: { 
+      Scale: { x: 0.05, y: 0.05, z: 0.05 },
     }
   }
 };
