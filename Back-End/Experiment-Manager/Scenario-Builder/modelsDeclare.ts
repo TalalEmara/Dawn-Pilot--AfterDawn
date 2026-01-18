cc3da171dd6da01090c6e1bca234027052940e87
H252003 <mohamedhesham252003@gmail.com>
2026-01-18 11:18:50 +0200

    refactor: added the new tree model

--- Back-End/Experiment-Manager/Scenario-Builder/modelsDeclare.ts
+++ Back-End/Experiment-Manager/Scenario-Builder/modelsDeclare.ts
@@ -120,7 +120,8 @@ export const ModelDefinitions: Record<string, ModelDefinition> = {
  
     },
     normalizeFactors: {
-      Scale: { x: 0.005, y: 0.005, z: 0.005 },
+      Position: { x: 0, y: 1, z: 0 },
+      Scale: { x: 0.01, y: 0.01, z: 0.01 },
     }
   },
   Pole: {
